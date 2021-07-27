dcfefe16cf57b1c4fe69f6cb265577c1d616bf14
Soumya Mishra <soumya_rm@outlook.com>
2021-07-27 20:28:03 +0530

    static methods in the class

--- oops/classAndObj.py
+++ oops/classAndObj.py
@@ -1,4 +1,7 @@
 # class will be blueprint and Objects are instances of class
+import datetime
+
+
 class Employee:
     raise_amount = 1.04
     num_of_employee = 0
@@ -11,11 +14,18 @@ class Employee:
         Employee.num_of_employee += 1  # it will increment the employee count everytime we create an Obj
         # and it will constant for all objects and not instance specific
 
+    # since self is added automatically , this type method is called instance methods
     def return_full_name(self):
         return '{} {}'.format(self.first_name, self.last_name)
 
     # regular method in a class automatically takes instance as first argument
     # _self_ points to instance as first argument
+
+    def apply_raise(self):
+        # if we apply Employee.raise_amount -> will take the class attribute value
+        # if we apply self -> it will take instance variable value for that particular instance
+        # if instance is not defined -> then will pick class variable value
+        self.pay = self.pay * self.raise_amount
 
     """
     To turn a regular method into a class method
@@ -36,8 +46,18 @@ class Employee:
         first_name, last_name, pay = input_string.split('-')
         return cls(first_name, last_name, pay)
 
-    def apply_raise(self):
-        # if we apply Employee.raise_amount -> will take the class attribute value
-        # if we apply self -> it will take instance variable value for that particular instance
-        # if instance is not defined -> then will pick class variable value
-        self.pay = self.pay * self.raise_amount
+    """
+    Static methods does not add anything as argument , neither cls nor self
+    They behave like normal functions and but since we put it in a class as it has some logical connections
+    static method has a decorator called @staticmethod
+    A method can be declared as static method if we don't access the instance or the class anywhere with in the function
+    This static method is getting executed in classMethodsAndStaticMethods.py
+    """
+
+    @staticmethod
+    # we are taking a day as input and returning as whether working day or not
+    def is_working_day(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        else:
+            return True
--- oops/classMethodsAndStaticMethods.py
+++ oops/classMethodsAndStaticMethods.py
@@ -1,4 +1,5 @@
 from classAndObj import Employee
+import datetime
 
 print(Employee.num_of_employee)
 
@@ -29,7 +30,28 @@ print(employee_1.raise_amount)
 print(employee_2.raise_amount)
 
 # creating an Instance using alternative constructor
+
 employee_3 = Employee.from_string('john-doe-45000')
 
 print(Employee.num_of_employee)
 print(employee_3.__dict__)
+
+employee_4 = Employee.from_string('john-Ham-55000')
+print(employee_4.__dict__)
+print(Employee.num_of_employee)
+
+print(employee_1.__dict__)
+
+# this will get reassigned and that will treated as a new Object
+# and the employee_1 originally constructed is an abandoned object now
+employee_1 = Employee('Nazmul', 'hossain', 6200)
+print(Employee.num_of_employee)
+print(employee_1.__dict__)
+
+# fromtimestamp() is an example of alternative constructor class method
+
+# executing static method
+my_date = datetime.date(2016, 7, 10)  # sunday
+print(Employee.is_working_day(my_date))
+my_date = datetime.date(2016, 7, 12)  # tuesday
+print(Employee.is_working_day(my_date))
